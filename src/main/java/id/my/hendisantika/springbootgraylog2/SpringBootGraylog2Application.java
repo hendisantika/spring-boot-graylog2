64da86f29085f523068268162ba276d0f648bb72
hendisantika <hendisantika@yahoo.co.id>
2025-01-05 06:17:40 +0700

    Create Spring Boot App🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/springbootgraylog2/SpringBootGraylog2Application.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springbootgraylog2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGraylog2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootGraylog2Application.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/springbootgraylog2/SpringBootGraylog2ApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springbootgraylog2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGraylog2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
